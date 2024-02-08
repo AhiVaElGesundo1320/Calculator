f6ee96f9760ca9bab68026a41a4c43f812440662
Ethan <ethangmg@gmail.com>
2024-02-07 18:30:55 -0600

    boton themeMode

--- src/app/components/[id].js
+++ src/app/components/[id].js
@@ -30,8 +30,9 @@ export const Calculator = () => {
   //className="bg-teal-300 my-3 w-52 h-14 rounded-t-lg flex dlex-col items-end" bg-teal-950
 
   return (
-    <div className="bg-teal-300 dark:bg-teal-800 h-3/4 w-72 rounded-lg flex flex-col items-center">
-      <div className="bg-teal-50 my-3 dark:bg-teal-300 w-64  h-14 rounded-t-lg flex flex-col items-center">
+    <div className='bg-teal-300 dark:bg-teal-800 light:bg-teal-300 h-3/4 w-72 rounded-lg flex flex-col items-center' 
+    >
+      <div className=" bg-teal-50 my-3 dark:bg-teal-300 w-64  h-14 rounded-t-lg flex flex-col items-center">
         <input
           className="bg-transparent w-52 h-14 text-teal-950  flex items-end text-end active:none focus:outline-none"
           type="text"
